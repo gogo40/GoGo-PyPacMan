16865e9286f93ef16c66bf507af0f84aedd4487f
gogo40 <pericles.raskolnikoff@gmail.com>
2013-06-29 14:24:23 -0300

    Criada estrutura do repositorio

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='PyPacMan',
+    version='0.1.0',
+    author='Pericles Lopes Machado',
+    author_email='pericles.raskolnikoff@gmail.com',
+    packages=['gui', 'IO', 'AI', 'game'],
+    scripts=[],
+    url='https://github.com/gogo40/PyPacMan',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
