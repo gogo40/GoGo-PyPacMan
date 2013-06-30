6892c882a328fae366ac91f9c844308678b763d5
gogo40 <pericles.raskolnikoff@gmail.com>
2013-06-29 22:08:49 -0300

    Adicionada colisao e mapa infinito

--- build/scripts-2.7/window.py
+++ build/scripts-2.7/window.py
@@ -11,6 +11,9 @@ class Window:
 		self.speed = self.dim_cell
 
 		#dimensao da tela
+		self.n_cell = len(G);
+		self.m_cell = len(G[0]);
+
 		self.size = self.width, self.height = self.dim_cell * len(G[0]), self.dim_cell * len(G)
 		self.screen = pygame.display.set_mode(self.size)
 
@@ -44,7 +47,9 @@ class Window:
 		self.parede = pygame.transform.smoothscale(self.parede, (self.dim_cell, self.dim_cell))
 
 		self.position = self.pacmano.get_rect()
+		self.real_pos_pac_man = [0, 0] 
 		self.d = [0, 0]
+		self.dr = [0, 0]
 		self.addGrid(G)
 
 	def addGrid(self, G):
@@ -63,6 +68,7 @@ class Window:
 					self.screen.blit(self.parede, self.position)
 				elif self.G[y][x] == '+':
 					self.position = self.pacman.get_rect()
+					self.real_pos_pac_man = [y, x]
 					self.position_pac_man = self.position.move(x * self.dim_cell, y * self.dim_cell)
 					self.screen.blit(self.pacman, self.position_pac_man)
 				elif self.G[y][x] == 'x' or self.G[y][x] == '*':
@@ -88,36 +94,65 @@ class Window:
 					if event.key == pygame.K_LEFT:
 						self.d[0] = -self.speed
 						self.d[1] = 0
+
+						self.dr[1] = -1
+						self.dr[0] = 0
+
 						self.pacman = pygame.transform.rotate(self.pacmano, 360)
 					elif event.key == pygame.K_RIGHT:
 						self.d[0] = self.speed
 						self.d[1] = 0
+
+						self.dr[1] = 1
+						self.dr[0] = 0
+
 						self.pacman = pygame.transform.rotate(self.pacmano, 180)
 					elif event.key == pygame.K_UP:
 						self.d[0] = 0
 						self.d[1] = -self.speed
+
+						self.dr[1] = 0
+						self.dr[0] = -1
+
 						self.pacman = pygame.transform.rotate(self.pacmano, 270)
 					elif event.key == pygame.K_DOWN:
 						self.d[0] = 0
 						self.d[1] = self.speed
+
+						self.dr[1] = 0
+						self.dr[0] = 1
+
 						self.pacman = pygame.transform.rotate(self.pacmano, 90)
 				if event.type == pygame.KEYUP:
 					self.d = [0, 0]
+					self.dr = [0, 0]
 
 			
 
 			#Atualiza posicao do pacman
 			self.position = self.position_pac_man 
-
-			if (self.position[0] + self.d[0] >= 0 and 
-				self.position[0] + self.d[0] <= self.width - self.position[2] and
-				self.position[1] + self.d[1] >= 0 and 
-				self.position[1] + self.d[1] <= self.height - self.position[3]):
-
+			real_pos = self.real_pos_pac_man
+			x = (real_pos[0] + self.dr[0] + self.n_cell) % self.n_cell;
+			y = (real_pos[1] + self.dr[1] + self.m_cell) % self.m_cell;
+			
+			if self.G[x][y] == '.' or self.G[x][y] == 'o':
+				self.real_pos_pac_man = [x, y]
 				self.screen.blit(self.empty, self.position)
+				
+				l = list(self.G[real_pos[0]]);
+				l[real_pos[1]]='.';
+				self.G[real_pos[0]]="".join(l);
 
-				self.position_pac_man = self.position.move(self.d)	
-	
+				l = list(self.G[x]);
+				l[y]='+';
+				self.G[x]="".join(l);
+
+				N = self.dim_cell * self.n_cell
+				M = self.dim_cell * self.m_cell
+
+				self.position_pac_man[0] = (self.position[0] + self.d[0] + M) % M
+				self.position_pac_man[1] = (self.position[1] + self.d[1] + N) % N
+
 				self.screen.blit(self.pacman, self.position_pac_man)
 
 				pygame.display.flip()
